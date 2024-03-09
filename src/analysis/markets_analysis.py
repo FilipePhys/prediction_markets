bc24ef52764c674af3c7bcdfd24b033da871d91e
Augusto Galego <augustomirandagalego@gmail.com>
2024-03-09 12:26:48 +0100

    .

--- src/analysis/markets_analysis.py
+++ src/analysis/markets_analysis.py
@@ -2,17 +2,19 @@ from futuur.market import FutuurMarket
 from futuur.futuur_api import FutuurAPI
 from manifold.manifold_api import ManifoldAPI
 import settings
+from analysis.analyzer import Analizer
 
 def analyze():
-    markets = FutuurMarket()
-    for m in markets.markets:
-        print(m)
 
-    manifold_api = ManifoldAPI()
-    manifold_market = manifold_api.get_market_by_slug()
+    analyzer = Analizer()
 
-    futuur_api = FutuurAPI(settings.FUTUUR_PUBLIC_KEY, settings.FUTUUR_PUBLIC_KEY)
-    futuur_market = futuur_api.get_market(133793)
+    analyzer.display_arbitrage()
 
-    print("Mani market: ", manifold_market)
-    print("Futuur market: ", manifold_market)
+                
+
+    
+
+    
+
+
+
